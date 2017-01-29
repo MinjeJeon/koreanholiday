acdf2792d9f8128f8d670322208fcbdf0132559d
Minje Jeon <i@minje.kr>
2017-01-29 22:59:39 +0900

    초기 개발

--- koreanholiday/koreanholiday.py
+++ koreanholiday/koreanholiday.py
@@ -3,53 +3,58 @@ import tempfile
 
 from lunardate import LunarDate
 
+
 class Holiday:
 
     HOLIDAYS_NAME = ['newyearsday', 'lunarnewyearsday', 'independencemovementday',
                      'arborday', 'childrensday', 'buddhasbirthday',
                      'memorialday', 'constitutionday', 'liberationday',
-                     'koreanthanksgivingday', 'nationalfoundationday', 'hangulday',
+                     'koreanthanksgiving', 'nationalfoundationday', 'hangulday',
                      'christmas']
     DESCRIPTION = {
-        'en' : ["New Year's Day(Sinjeong)", "Lunar New Year's Day(Seolnal)", "Independence Movement Day(Samiljeol)",
-                "Korean Arbor Day(Sikmokil)", "Children's Day(Eorininal)", ],
-        'ko' : ['신정', '설날', '삼일절',
-                '식목일', '어린이날', '석가탄신일',
-                '현충일', '제헌절', '광복절',
-                '추석', '개천절', '한글날',
-                '크리스마스']
-        } 
+        'en': ["New Year's Day(Sinjeong)", "Lunar New Year's Day(Seolnal)", "Independence Movement Day(Samiljeol)",
+               "Korean Arbor Day(Sikmokil)", "Children's Day(Eorininal)", "Buddha's Birthday(Seokgatansinil)",
+               "Memorial Day(Hyeonchung-il)", "Constitution Day(Jeheonjeol)", "Liberation Day(Gwangbokjeol)",
+               "Korean Thanksgiving(Chuseok)", "National Foundation Day(Gaecheonjeol)", "Hangul Day(Hangeulnal)",
+               "Christmas"],
+        'ko': ['신정', '설날', '삼일절',
+               '식목일', '어린이날', '석가탄신일',
+               '현충일', '제헌절', '광복절',
+               '추석', '개천절', '한글날',
+               '크리스마스']
+    }
 
     def __init__(self, online=True, refresh=False, lang='en'):
         self.online = online
         self.timestamp = datetime.datetime.now()
-        self
+        self.desc = {k: v for k, v in zip(
+            self.HOLIDAYS_NAME, self.DESCRIPTION[lang])}
 
         self._special = None
-        self._holidays_except_alternative = {}
+        self._holidays_except_substitute = {}
         self._holidays = {}
-        
+
         if refresh:
-           self.refresh()
-    
+            self.refresh()
+
     @property
     def special(self):
         if self.special is None:
             self._special = self._get_special(self.online)
         return self._special
-    
+
     @property
     def today(self):
         return datetime.date.today()
-    
+
     @property
     def thisyear(self):
         return datetime.date.today().year
-    
+
     @staticmethod
     def yesterday(date):
         return date - datetime.timedelta(days=1)
-    
+
     @staticmethod
     def tomorrow(date):
         return date + datetime.timedelta(days=1)
@@ -57,11 +62,11 @@ class Holiday:
     @staticmethod
     def threedays(date):
         return [date - datetime.timedelta(days=1), date, date + datetime.timedelta(days=1)]
-    
+
     def newyearsday(self, year=None, dayoff=False):
         year = year if year else self.thisyear
         return datetime.date(year, 1, 1)
-    
+
     def lunarnewyearsday(self, year=None, dayoff=False):
         year = year if year else self.thisyear
         theday = LunarDate(year, 1, 1).toSolarDate()
@@ -69,11 +74,11 @@ class Holiday:
             return self.threedays(theday)
         else:
             return theday
-        
+
     def independencemovementday(self, year=None, dayoff=False):
-        year = year if year else self.thisyear 
+        year = year if year else self.thisyear
         return datetime.date(year, 3, 1)
-    
+
     def arborday(self, year=None, dayoff=False):
         year = year if year else self.thisyear
         theday = datetime.date(year, 4, 5)
@@ -81,12 +86,12 @@ class Holiday:
             if 1948 <= year < 2006:
                 return theday
         else:
-            return None  
-    
+            return None
+
     def childrensday(self, year=None, dayoff=False):
         year = year if year else self.thisyear
         return datetime.date(year, 5, 5)
-    
+
     def buddhasbirthday(self, year=None, dayoff=False):
         year = year if year else self.thisyear
         return LunarDate(year, 4, 8).toSolarDate()
@@ -94,7 +99,7 @@ class Holiday:
     def memorialday(self, year=None, dayoff=False):
         year = year if year else self.thisyear
         return datetime.date(year, 6, 6)
-    
+
     def constitutionday(self, year=None, dayoff=False):
         year = year if year else self.thisyear
         theday = datetime.date(year, 7, 17)
@@ -103,46 +108,44 @@ class Holiday:
                 return theday
             else:
                 return None
-    
+
     def liberationday(self, year=None, dayoff=False):
         year = year if year else self.thisyear
         return datetime.date(year, 8, 15)
-    
-    def koreanthanksgivingday(self, year=None, dayoff=False):
+
+    def koreanthanksgiving(self, year=None, dayoff=False):
         year = year if year else self.thisyear
         theday = LunarDate(year, 8, 15).toSolarDate()
         if dayoff:
             return self.threedays(theday)
         else:
             return theday
-    
+
     def nationalfoundationday(self, year=None, dayoff=False):
         year = year if year else self.thisyear
         return datetime.date(year, 10, 3)
-    
+
     def hangulday(self, year=None, dayoff=False):
         year = year if year else self.thisyear
         return datetime.date(year, 10, 9)
-    
+
     def christmas(self, year=None, dayoff=False):
         year = year if year else self.thisyear
         return datetime.date(year, 12, 25)
 
+    # substitute holiday
 
-    #alternative holiday
-
-    def holidays_except_alternative(self, year=None):
+    def holidays_before_substitution(self, year=None):
         year = year if year else self.thisyear
 
     def isworkday(self, date):
         pass
 
-    def refresh(online=True):
+    def refresh(self, online=True):
         pass
-        
-    
+
     def _get_special(self, online, refresh=None):
         if self.online:
             pass
         else:
-            pass        
+            pass
